5c3cdcfffd40f29fd11076ad51890af0f2a20b42
Timothy Beck (백재량) <timothyjrbeck@gmail.com>
2019-04-18 16:18:08 +0900

    getJSON debugging

---
+++ data/items-dev.js
@@ -0,0 +1,34 @@
+var items = {
+    "tiles":[
+        {
+            "index": 0,
+            "name": "Empty"       
+        },
+        {
+            "index": 1,
+            "name": "Cannot Move Tile"       
+        },
+        {
+            "index": 2,
+            "name": "Conveyor belt"       
+        },
+        {
+            "index": 3,
+            "name": "Door"       
+        },
+        {
+            "index": 4,
+            "name": "TEST"       
+        }
+    ],
+    "obstacles":[
+        {
+            "index": 0,
+            "name": "Empty"       
+        },
+        {
+            "index": 1,
+            "name": "Cream"       
+        }
+    ]
+}
