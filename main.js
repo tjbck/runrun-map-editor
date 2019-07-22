a19423ed6e16145d683ff7d9bd21dff6c01a3c09
Timothy Jaeryang Beck <timothyjrbeck@gmail.com>
2019-07-23 01:00:48 +0900

    Updated main.js

--- main.js
+++ main.js
@@ -1,110 +1,100 @@
-fetch('./data/items.json')
-    .then(response => {
-        return response.json()
-    })
-    .then(items => {
-        var app = new Vue({
-            el: '#app',
-            data: {
-                mapDataJSON: '',
-                tileSelected: 0,
-                tileOptions: items.tiles,
-                tileBoard: [
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-                ],
-                obstacleSelected: 1,
-                obstacleOptions: items.obstacles,
-                obstacleBoardBase: [
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-                ],
-                obstacleBoards: [
-                    [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-                    ], [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-                    ], [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-                    ], [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-                    ]
-                ]
-            },
-            methods: {
-                addObstacleBoard: function () {
-                    this.obstacleBoards.push(JSON.parse(JSON.stringify(app.obstacleBoardBase)))
-                },
-                removeObstacleBoard: function () {
-                    this.obstacleBoards.splice(app.obstacleBoards.length - 1, 1)
-                },
-                exportToJson: function () {
-                    this.mapDataJSON = JSON.stringify({
-                        "T": this.tileBoard,
-                        "P": this.obstacleBoards
-                    })
-                },
-                downloadMap: function () {
-                    var text = document.getElementById("mapJsonData").value;
-                    var filename = "r" + Date.now() + ".json";
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: false,
+        mapDataJSON: '',
+        tileSelected: 0,
+        tileOptions: '',
+        tileBoard: [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ],
+        obstacleSelected: 1,
+        obstacleOptions: '',
+        obstacleBoardBase: [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ],
+        obstacleBoards: [
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ], [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ], [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ], [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+        ]
+    },
+    methods: {
+        addObstacleBoard: function () {
+            this.obstacleBoards.push(JSON.parse(JSON.stringify(app.obstacleBoardBase)))
+        },
+        removeObstacleBoard: function () {
+            this.obstacleBoards.splice(app.obstacleBoards.length - 1, 1)
+        },
+        exportToJson: function () {
+            this.mapDataJSON = JSON.stringify({
+                "T": this.tileBoard,
+                "P": this.obstacleBoards
+            })
+        },
+        downloadMap: function () {
+            var text = document.getElementById("mapJsonData").value;
+            var filename = "r" + Date.now() + ".json";
 
-                    var element = document.createElement('a');
-                    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-                    element.setAttribute('download', filename);
-                    element.style.display = 'none';
-                    document.body.appendChild(element);
-                    element.click();
-                    document.body.removeChild(element);
-                },
-                copyToClipboard: function () {
-                    var copyText = document.getElementById("mapJsonData");
-                    copyText.select();
-                    document.execCommand("copy");
-                    alert("Copied Map Data");
-                },
-                loadMap: function () {
-                    var mapDataJSON = JSON.parse(this.mapDataJSON)
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            element.setAttribute('download', filename);
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+        },
+        copyToClipboard: function () {
+            var copyText = document.getElementById("mapJsonData");
+            copyText.select();
+            document.execCommand("copy");
+            alert("Copied Map Data");
+        },
+        loadMap: function () {
+            var mapDataJSON = JSON.parse(this.mapDataJSON)
 
-                    this.tileBoard = mapDataJSON["T"]
-                    this.obstacleBoards = mapDataJSON["P"]
+            this.tileBoard = mapDataJSON["T"]
+            this.obstacleBoards = mapDataJSON["P"]
 
-                    alert('Map Loaded')
-                }
-            },
-            delimiters: ['[[', ']]']
-        })
-    })
-    .catch(err => {
-        console.log("An Error has Ocurred")
-    })
+            alert('Map Loaded')
+        }
+    },
+    mounted: async function () {
+        const response = await fetch('./data/items.json')
+        const items = await response.json()
 
-
-
-// COMMENTS BELOW
-
-// new Vue({ el: '#app', data: { message }, delimiters: ['[[', ']]'] });
-// Vue.set(app.obstacleBoards, app.obstacleBoards.length, JSON.parse(JSON.stringify(app.obstacleBoardBase)))
-// app.obstacleBoards.push(JSON.parse(JSON.stringify(app.obstacleBoardBase)))
-// app.obstacleBoards.splice(app.obstacleBoards.length - 1, 1)
+        this.items = true
+        this.tileOptions = items.tiles
+        this.obstacleOptions = items.obstacles
+    }
+})
